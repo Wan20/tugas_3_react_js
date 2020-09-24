dfc59fd3c3f81bb0808a66a5ee242430adcb82b0
Wandy <wandy20number@gmail.com>
2020-09-24 14:32:42 +0700

    Tugas 3 React Js Niomic
    Commit Tugas 3 React Js Niomic

---
+++ src/Page/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './index.css';
+
+const Header = () => {
+    return (
+      <div>
+        <ul className="nav navbar-nav navbar-middle navbar-text">
+          <li>Home</li>
+          <li>Product</li>
+          <li>Kontak</li>
+          <li>Tentang Kami</li>
+        </ul>
+      </div>
+    )
+}
+
+export default Header;
